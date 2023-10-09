c79a4a0a00c06e75620431fd84b67a04ea18ae45
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2023-10-09 18:15:32 +0600

    unique category solved

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({allCards}) => {
+    const uniqueCards = [...new Set(allCards.map(item => item.cardtype))];
     return (
         <div className=' bg-[#eeeded] pt-4'>
             <div className="wrapper">
                 <div className="card-grp flex items-center justify-center  font-medium ">
-                    <button className='border-2 rounded-md p-4 bg-[#9EDDFF] hover:bg-[#6499E9]'>Debit Cards</button>
-                    <button className='mx-4 border-2 rounded-md p-4  bg-[#9EDDFF] hover:bg-[#6499E9]'>Credit Cards</button>
-                    <button className='border-2 rounded-md p-4  bg-[#9EDDFF] hover:bg-[#6499E9]'>ATM Cards</button>
+                    {uniqueCards.map( (card,i) =>(
+                             <button key={i} 
+                             className='border-2  rounded-md p-4 bg-[#9EDDFF] hover:bg-[#6499E9]'>{card}</button>
+                             ))}
+                   
+                    {/* <button className='mx-4 border-2 rounded-md p-4  bg-[#9EDDFF] hover:bg-[#6499E9]'>Credit Cards</button>
+                    <button className='border-2 rounded-md p-4  bg-[#9EDDFF] hover:bg-[#6499E9]'>ATM Cards</button> */}
                 </div>
             </div>
         </div>
--- src/components/Selections/Selections.js
+++ src/components/Selections/Selections.js
@@ -12,13 +12,16 @@ const Selections = () => {
     const [selected, setSelected] = useState("");
     const [open, setOpen] = useState(false);
     const [selectedCategory, setSelectedCategory] = useState(null);
+    const [cardSelected, setCardSelected] = useState(null);
+
+    const uniqueCards = [...new Set(allInfos.map(item => item.cardtype))];
+    const uniqueCategorys = [...new Set(allInfos.map(item => item.category))];
 
     useEffect(() => {
-        fetch("data.json") 
-    //https://restcountries.com/v2/all?fields=name
+        fetch("data.json")
+            //https://restcountries.com/v2/all?fields=name
             .then((res) => res.json())
             .then((data) => {
-                console.log(data)
                 setAllInfos(data);
             });
     }, []);
@@ -26,72 +29,86 @@ const Selections = () => {
     const handleCategorySelect = (cat) => {
         setSelectedCategory(cat);
         // setSelectedBank(null);
-      };
+    };
+    const handleSelectedCard = (type) => {
+        setCardSelected(type);
+        // setSelectedBank(null);
+    };
 
     return (
         <div>
-        <div className='mx-auto w-full px-10 py-5  bg-slate-100'>
-            <div className="wrapper flex flex-cols-2   justify-between items-center font-semibold">
-                <div className='w-52 relative'>
-                    <div onClick={() => setOpen(!open)} className={`flex items-center justify-between text-lg  bg-slate-200 rounded-sm px-2 ${!selected && "text-gray-600"}`}>
-                        
-                        <p className=''>{selected
-                        ? selected?.length > 15
-                        ? selected?.substring(0,15)  + "..."
-                        : selected 
-                        : "Category"}</p>
-                        <p className=''>< AiFillCaretDown size={20} className={`${open && "rotate-180"}`} /></p>
+            <div className='mx-auto w-full px-10 py-5  bg-slate-100'>
+                <div className="wrapper flex flex-cols-2   justify-between items-center font-semibold">
+                    <div className='w-52 relative'>
+                        <div onClick={() => setOpen(!open)} className={`flex items-center justify-between text-lg  bg-slate-200 rounded-sm px-2 ${!selected && "text-gray-600"}`}>
+
+                            <p className=''>{selected
+                                ? selected?.length > 15
+                                    ? selected?.substring(0, 15) + "..."
+                                    : selected
+                                : "Category"}</p>
+                            <p className=''>< AiFillCaretDown size={20} className={`${open && "rotate-180"}`} /></p>
+                        </div>
+                        <ul className={`absolute bg-slate-200 w-54 top-10 rounded-sm text-left overflow-y-auto ${open ? " max-h-52" : " max-h-0"
+                            }`}>
+
+                            {
+                                //  allInfos.map((alInfo, i) => (
+                                    uniqueCategorys.map((alInfo, i) => (
+                                    // Bappy is okkkkk
+                                    <li key={i}
+                                        className={`hover:bg-sky-400 hover:text-white text-sm cursor-pointer px-2
+                                 ${alInfo?.toLowerCase() === selected?.toLowerCase() &&
+                                            "bg-sky-600 text-white"
+                                            }`}
+                                        onClick={() => {
+                                            // if(country?.name?.toLowerCase() !== selected.toLowerCase() ){
+                                            //     setSelected(country.name);
+                                            // }
+                                            setSelected(alInfo);
+                                            setOpen(false)
+                                            handleCategorySelect(alInfo)
+                                        }}
+                                    >
+                                        {alInfo}
+                                    </li>
+                                ))
+                            }
+                        </ul>
+
                     </div>
-                    <ul className={`absolute bg-slate-200 w-54 top-10 rounded-sm text-left overflow-y-auto ${
-                        open ? " max-h-52" : " max-h-0"
-                        }`}>
+                    <div className='flex items-center justify-between text-lg w-52 bg-slate-200 rounded-md px-2'>
+                        <p className=''>Bank</p>
+                        <p className=''><AiFillCaretDown size={20} /></p>
+                    </div>
+                    <div className='flex items-center justify-between bg-slate-200 rounded-md w-52 text-lg px-2'>
+                        <p className=''>Review</p>
+                        <p><MdOutlineReviews size={20} /></p>
+                    </div>
+                    <div className='flex items-center justify-between text-lg bg-slate-200 rounded-md w-52 px-2'>
+                        <p className=''>Search</p>
+                        <p><FiSearch size={20}></FiSearch></p>
+                    </div>
+                </div>
+            </div>
 
-                        {
-                            allInfos.map((alInfo,i) => (
-                                
-                                <li key={i}
-                                 className={`hover:bg-sky-400 hover:text-white text-sm cursor-pointer px-2
-                                 ${ alInfo?.category?.type.toLowerCase() === selected?.toLowerCase() &&
-                                    "bg-sky-600 text-white"
-                                 }`}
-                                 onClick={() =>{
-                                    // if(country?.name?.toLowerCase() !== selected.toLowerCase() ){
-                                    //     setSelected(country.name);
-                                    // }
-                                    setSelected(alInfo.category.type);
-                                    setOpen(false)
-                                    handleCategorySelect(alInfo)
-                                 }}
-                                 >
-                                    {alInfo?.category.type}
-                                </li>
-                            ))
-                        }
-                    </ul>
 
-                </div>
-                <div className='flex items-center justify-between text-lg w-52 bg-slate-200 rounded-md px-2'>
-                    <p className=''>Bank</p>
-                    <p className=''><AiFillCaretDown size={20} /></p>
-                </div>
-                <div className='flex items-center justify-between bg-slate-200 rounded-md w-52 text-lg px-2'>
-                    <p className=''>Review</p>
-                    <p><MdOutlineReviews size={20} /></p>
-                </div>
-                <div className='flex items-center justify-between text-lg bg-slate-200 rounded-md w-52 px-2'>
-                    <p className=''>Search</p>
-                    <p><FiSearch size={20}></FiSearch></p>
+            {/* <Card allCards={allInfos} handleCard = {}></Card> */}
+            <div className=' bg-[#eeeded] pt-4'>
+            <div className="wrapper">
+                <div className="card-grp flex items-center justify-center  font-medium ">
+                    {uniqueCards.map( (card,i) =>(
+                             <button key={i} onClick={() =>handleSelectedCard(card)}
+                             className='border-2  rounded-md p-4 bg-[#9EDDFF] hover:bg-[#6499E9]'>{card}</button>
+                             ))}
                 </div>
             </div>
         </div>
-        
-        
-        <Card ></Card>
-        <TopBanner></TopBanner>
-        <div>
-        {selectedCategory && <CategoryComponent category={allInfos} />}
-        {/* {selectedBank && <BankComponent bank={selectedBank} />} */}
-      </div>
+            <TopBanner></TopBanner>
+            <div>
+                {selectedCategory && <CategoryComponent category={allInfos} selected={selected} cardSelected={cardSelected}  />}
+                {/* {selectedBank && <BankComponent bank={selectedBank} />} */}
+            </div>
         </div>
     );
 };
