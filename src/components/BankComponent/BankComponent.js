cf0d63f3164d76b587feaf0a40f30851d2b510c3
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2023-10-10 18:07:26 +0600

    bankcomponent and bankselect add

--- src/components/BankComponent/BankComponent.js
+++ src/components/BankComponent/BankComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import vertical_banner2 from '../../assets/images/vertical_banner.svg'
-const CategoryComponent = ({ allInfos, selected, filterCards }) => {
+const BankComponent = ({ allInfos, selected, filterCards }) => {
   return (
     <div>
       <div className='mt-14 mx-4 flex flex-col md:flex-row justify-between'>
@@ -16,13 +16,13 @@ const CategoryComponent = ({ allInfos, selected, filterCards }) => {
         <div className="left-seection w-4/5 grid grid-cols-1 h-52 md:grid-cols-2 gap-4 px-4">
           {/* Create a 3x3 grid of information */}
           {/*  singleinfo.filter( singleinfo.category===selected) */}
-          {allInfos.filter((singleInfo) => singleInfo.category === selected)
+          {allInfos.filter((singleInfo) => singleInfo.bankfullname === selected)
             .map((singleInfo, i) => (
 
               <div key={i} className="info-item py-8 flex flex-col lg:flex-row  border border-solid border-amber-500 rounded-md px-3
               bg-[#9EDDFF]">
-                <div className='text-lg md:text-xl lg:text-2xl font-serif font-semibold md:font-bold lg:tracking-wide  mr-2 md:mr-4 lg:mr-8 uppercase'>{singleInfo.name} <br />{singleInfo.category}</div>
-                <div className='font-medium  text-justify'>Enjoy <span className='font-bold text-red-500 text-lg'>{singleInfo.discountdetail}</span> &<br /><span className=''>Exciting Deals</span> <br /> with your<span className='font-bold'> {singleInfo.bankshortname} {singleInfo.cardtype}</span> <p></p></div>
+                <div className='text-lg md:text-xl lg:text-2xl font-serif font-semibold md:font-bold lg:tracking-wide  mr-2 md:mr-4 lg:mr-8 uppercase'>{singleInfo.bankshortname} <br />OFFER!!</div>
+                <div className='font-medium  text-justify'>Enjoy <span className='font-bold text-red-500 text-lg'>{singleInfo.discountdetail}</span> &<br /><span className=''>Exciting Deals</span> <br /> with your<span className=''> {singleInfo.bankshortname} {singleInfo.cardtype}</span> <p></p></div>
                 <div className='font-medium  lg:ml-8 '> <span className='text-red-500 font-bold uppercase text-xl'>{singleInfo.bankshortname}</span> & <span className='text-blue-800 font-bold uppercase text-xl'>{singleInfo.cardacategory}</span></div>
               </div>
 
@@ -43,4 +43,4 @@ const CategoryComponent = ({ allInfos, selected, filterCards }) => {
   );
 };
 
-export default CategoryComponent;
+export default BankComponent;
--- src/components/Selections/Selections.js
+++ src/components/Selections/Selections.js
@@ -5,17 +5,23 @@ import { MdOutlineReviews } from "react-icons/md";
 import TopBanner from '../TopBanner/TopBanner';
 import Card from '../Card/Card';
 import CategoryComponent from '../CategoryComponent/CategoryComponent';
+import BankComponent from '../BankComponent/BankComponent';
 // import AllInfo from '../../data.json'
 
 const Selections = () => {
     const [allInfos, setAllInfos] = useState([])
+    // const [items, setItems] = useState([])
     const [selected, setSelected] = useState("");
-    const [open, setOpen] = useState(false);
+    const [bankname, setBankname] = useState("");
+    const [openCat, setOpenCat] = useState(false);
+    const [openBank, setOpenBank] = useState(false);
     const [selectedCategory, setSelectedCategory] = useState(null);
-    const [cardSelected, setCardSelected] = useState(null);
+    const [selectedBank, setSelectedBank] = useState(null);
+    // const [cardSelected, setCardSelected] = useState(null);
 
     const uniqueCards = [...new Set(allInfos.map(item => item.cardtype))];
     const uniqueCategorys = [...new Set(allInfos.map(item => item.category))];
+    const uniqueBank = [...new Set(allInfos.map(item => item.bankfullname))];
 
     useEffect(() => {
         fetch("data.json")
@@ -23,38 +29,53 @@ const Selections = () => {
             .then((res) => res.json())
             .then((data) => {
                 setAllInfos(data);
+                // setItems(data);
             });
     }, []);
 
     const handleCategorySelect = (cat) => {
         setSelectedCategory(cat);
-        // setSelectedBank(null);
+        setSelectedBank(null);
+        setBankname("")
+        
     };
-    const handleSelectedCard = (type) => {
-        setCardSelected(type);
-        // setSelectedBank(null);
+    const handleBankSelect = (cat) => {
+        setSelectedBank(cat);
+        setSelected("")
+        setSelectedCategory(null);
+        
+
     };
+    // const handleSelectedCard = (type) => {
+    //     setCardSelected(type);
+    //     // setSelectedBank(null);
+    // };
+
+    // const filterCards = (types) => {
+    //     const newItems = allInfos.filter((newval) => newval.cardtype === types)
+    //     setItems(newItems)
+    // }
 
     return (
         <div>
             <div className='mx-auto w-full px-10 py-5  bg-slate-100'>
                 <div className="wrapper flex flex-cols-2   justify-between items-center font-semibold">
                     <div className='w-52 relative'>
-                        <div onClick={() => setOpen(!open)} className={`flex items-center justify-between text-lg  bg-slate-200 rounded-sm px-2 ${!selected && "text-gray-600"}`}>
+                        <div onClick={() => setOpenCat(!openCat)} className={`flex items-center justify-between text-lg  bg-slate-200 rounded-sm px-2 ${!selected && "text-gray-600"}`}>
 
                             <p className=''>{selected
                                 ? selected?.length > 15
                                     ? selected?.substring(0, 15) + "..."
                                     : selected
                                 : "Category"}</p>
-                            <p className=''>< AiFillCaretDown size={20} className={`${open && "rotate-180"}`} /></p>
+                            <p className=''>< AiFillCaretDown size={20} className={`${openCat && "rotate-180"}`} /></p>
                         </div>
-                        <ul className={`absolute bg-slate-200 w-54 top-10 rounded-sm text-left overflow-y-auto ${open ? " max-h-52" : " max-h-0"
+                        <ul className={`absolute bg-slate-200 w-54 top-10 rounded-sm text-left overflow-y-auto ${openCat ? " max-h-52" : " max-h-0"
                             }`}>
 
                             {
                                 //  allInfos.map((alInfo, i) => (
-                                    uniqueCategorys.map((alInfo, i) => (
+                                uniqueCategorys.map((alInfo, i) => (
                                     // Bappy is okkkkk
                                     <li key={i}
                                         className={`hover:bg-sky-400 hover:text-white text-sm cursor-pointer px-2
@@ -66,7 +87,7 @@ const Selections = () => {
                                             //     setSelected(country.name);
                                             // }
                                             setSelected(alInfo);
-                                            setOpen(false)
+                                            setOpenCat(false)
                                             handleCategorySelect(alInfo)
                                         }}
                                     >
@@ -77,10 +98,45 @@ const Selections = () => {
                         </ul>
 
                     </div>
-                    <div className='flex items-center justify-between text-lg w-52 bg-slate-200 rounded-md px-2'>
+                    <div className='w-52 relative'>
+                        <div onClick={() => setOpenBank(!openBank)} className={`flex items-center justify-between text-lg  bg-slate-200 rounded-sm px-2 ${!bankname && "text-gray-600"}`}>
+
+                            <p className=''>{bankname
+                                ? bankname?.length > 15
+                                    ? bankname?.substring(0, 15) + "..."
+                                    : bankname
+                                : "Bank"}</p>
+                            <p className=''>< AiFillCaretDown size={20} className={`${openBank && "rotate-180"}`} /></p>
+                        </div>
+                        <ul className={`absolute bg-slate-200 w-54 top-10 rounded-sm text-left overflow-y-auto ${openBank ? " max-h-52" : " max-h-0"
+                            }`}>
+
+                            {
+                                //  allInfos.map((alInfo, i) => (
+                                uniqueBank.map((alInfo, i) => (
+                                    // Bappy is okkkkk
+                                    <li key={i}
+                                        className={`hover:bg-sky-400 hover:text-white text-sm cursor-pointer px-2
+                                 ${alInfo?.toLowerCase() === bankname?.toLowerCase() &&
+                                            "bg-sky-600 text-white"
+                                            }`}
+                                        onClick={() => {
+                                            setBankname(alInfo);
+                                            setOpenBank(false)
+                                            handleBankSelect(alInfo)
+                                        }}
+                                    >
+                                        {alInfo}
+                                    </li>
+                                ))
+                            }
+                        </ul>
+
+                    </div>
+                    {/* <div className='flex items-center justify-between text-lg w-52 bg-slate-200 rounded-md px-2'>
                         <p className=''>Bank</p>
                         <p className=''><AiFillCaretDown size={20} /></p>
-                    </div>
+                    </div> */}
                     <div className='flex items-center justify-between bg-slate-200 rounded-md w-52 text-lg px-2'>
                         <p className=''>Review</p>
                         <p><MdOutlineReviews size={20} /></p>
@@ -95,19 +151,20 @@ const Selections = () => {
 
             {/* <Card allCards={allInfos} handleCard = {}></Card> */}
             <div className=' bg-[#eeeded] pt-4'>
-            <div className="wrapper">
-                <div className="card-grp flex items-center justify-center  font-medium ">
-                    {uniqueCards.map( (card,i) =>(
-                             <button key={i} onClick={() =>handleSelectedCard(card)}
-                             className='border-2  rounded-md p-4 bg-[#9EDDFF] hover:bg-[#6499E9]'>{card}</button>
-                             ))}
+                <div className="wrapper">
+                    <div className="card-grp flex items-center justify-center  font-medium ">
+                        {uniqueCards.map((card, i) => (
+                            <button key={i}
+                            //  onClick={() => filterCards(card)}
+                                className='border-2  rounded-md p-4 bg-[#9EDDFF] hover:bg-[#6499E9]'>{card}</button>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
             <TopBanner></TopBanner>
             <div>
-                {selectedCategory && <CategoryComponent category={allInfos} selected={selected} cardSelected={cardSelected}  />}
-                {/* {selectedBank && <BankComponent bank={selectedBank} />} */}
+                {selectedCategory && <CategoryComponent allInfos={allInfos} selected={selected} />}
+                {selectedBank && <BankComponent allInfos={allInfos}  selected={bankname} />}
             </div>
         </div>
     );
